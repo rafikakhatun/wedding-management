4b829337efeaca7d524948bc459b1e60b6d23955
rafika khatun <rafikakhatun607@gmail.com>
2025-09-11 11:45:51 +0530

    create hero component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './component/Navbar'
+import Hero from './component/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
+    <Hero></Hero>
       
         
     </>
