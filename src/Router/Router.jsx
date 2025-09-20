e4e7d2518cb344f38f26ba751f99296f39dcb93b
rafika khatun <rafikakhatun607@gmail.com>
2025-09-20 20:52:41 +0530

    feat(router): add child routes for Dashboard (default and User)

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -8,10 +8,22 @@ export const router = createBrowserRouter([
         path:"/",
         element:<Home></Home>
     },
-    
+
      {
      path: '/admin/dashboard',
         element: <DashboardLayout />,
+
+        children: [
+          {
+                index: true, // '/admin/dashboard' এর জন্য ডিফল্ট পেজ
+                element: <DashboardPage />,
+            },
+            {
+                path: 'User',
+                element: <User></User>
+
+            }
+        ]
       
     }
 
