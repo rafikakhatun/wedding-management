2e82d7d902b06120e062b51a00f93ab7fc86e77f
rafika khatun <rafikakhatun607@gmail.com>
2025-09-20 21:18:02 +0530

    fix: import RouterProvider from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './page/Home.jsx'
-import { RouterProvider } from 'react-router'
 import { router } from './Router/Router.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
