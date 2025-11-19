79a994f975ba080824a7d3a4accb0483581c6ffb
rafika khatun <rafikakhatun607@gmail.com>
2025-11-19 18:53:25 +0530

    feat: add export handlers to PDF and Excel buttons

--- src/dashboard/UserPage.jsx
+++ src/dashboard/UserPage.jsx
@@ -12,12 +12,12 @@ const UserPage = () => {
                 <div className='flex flex-col md:flex-row justify-end items-center mb-6 space-y-4 md:space-y-0'>
                     <div className='flex space-x-3'> 
                         {/*pdf button */}
-                        <button className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
+                        <button onClick={()=>handleExport('pdf')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
                             <span>Export PDF</span>
                         </button>
 
                         {/*excel button*/}
-                        <button className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
+                        <button onClick={()=>handleExport('excel')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
                             <span>Export Excel</span>
                         </button>
                     </div>
