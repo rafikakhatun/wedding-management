12e8c150abf818bd419186ae56f7466a981bf472
rafika khatun <rafikakhatun607@gmail.com>
2025-11-19 19:11:13 +0530

    feat: implement initial export handler logic

--- src/dashboard/UserPage.jsx
+++ src/dashboard/UserPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const UserPage = () => {
+
+    // pdf and excel logic
+     const handleExport = (name)=>{
+        alert(name)
+        console.log(name)
+            
+        }
+
+
     return (
+
+
+
+       
 
         <>
             {/*User Table controler*/}
@@ -12,12 +25,12 @@ const UserPage = () => {
                 <div className='flex flex-col md:flex-row justify-end items-center mb-6 space-y-4 md:space-y-0'>
                     <div className='flex space-x-3'> 
                         {/*pdf button */}
-                        <button onClick={()=>handleExport('pdf')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
+                        <button onClick={()=>handleExport('PDF')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
                             <span>Export PDF</span>
                         </button>
 
                         {/*excel button*/}
-                        <button onClick={()=>handleExport('excel')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
+                        <button onClick={()=>handleExport('Excel')} className='flex items-center space-x-2 px-4 py-2 bg-slate-100 text-slate-600 rounded-lg hover:bg-slate-300 transition '>
                             <span>Export Excel</span>
                         </button>
                     </div>
